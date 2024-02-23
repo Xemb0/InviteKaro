a7df258eebe4efa389b34f7d6ffedd9f4fbf6a15
aman1455 <amannegi1140@gmail.com>
2024-02-23 14:19:56 +0530

    navbar

--- InviteKaro/src/component/Header/Header.jsx
+++ InviteKaro/src/component/Header/Header.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Header.css"
 import { NavLink } from 'react-router-dom'
+import { LuMenuSquare } from "react-icons/lu";
 
 function Header() {
+  const [open , setOpen] = useState(false)
+  console.log(open);
   return (
     <div className='nav'>
-      <div className='left'>InviteKaro</div>
+      <div className='left'><div>InviteKaro</div>
+      <div className='toggle' onClick={()=>setOpen(!open)}><LuMenuSquare /></div>
+      </div>
+      
       <div className='mid'>
-      <ul>
+      <ul className={open ? "open" : ""}>
   <li><NavLink exact to={'/'} >Home</NavLink></li>
   <li><NavLink to={'/wedding'}>Wdding</NavLink></li>
   <li><NavLink to={'/birthday'}> Birthday</NavLink></li>
   <li><NavLink to={'/reception'}>Reception</NavLink></li>
 </ul>
         </div>
-      <div className='right'>more</div>
+      <div className='right'>
+        <ul className={open ? "open" : ""}>
+          <li><NavLink to={'/all'}>More</NavLink></li>
+        </ul>
+      </div>
     </div>
   )
 }
